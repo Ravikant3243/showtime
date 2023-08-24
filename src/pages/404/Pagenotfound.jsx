c6017c7879e56db5ea06dc2862283dc55e0a75a0
Ravikant Yadav <ravikantresonite@gmail.com>
2023-08-24 18:26:40 +0530

    Final commit showtime completed

--- src/pages/404/Pagenotfound.jsx
+++ src/pages/404/Pagenotfound.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import "./styles.scss";
-function Pagenotfound() {
-  return (
-    <div>Pagenotfound</div>
-  )
-}
+import React from "react";
 
-export default Pagenotfound
+import "./style.scss";
+
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default PageNotFound;
--- src/pages/serachResult/SerachResult.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
- import "./styles.scss";
-function SerachResult() {
-  return (
-    <div>SerachResult</div>
-  )
-}
-
-export default SerachResult
